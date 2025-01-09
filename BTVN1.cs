dfa1f2e1553c6715204965d08be074e4e4cb6ef5
HIKHANHVY06 <vytran.31241025351@st.ueh.edu.vn>
2025-01-09 14:35:40 +0700

    Update BTVN1.cs

--- BTVN1.cs
+++ BTVN1.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
-namespace ConsoleApp4
+namespace ConsoleApp
 {
     internal class BTVN
     {
